97be261aecfe76f61db8ebe1fb5f4e975bd4de15
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-11-26 01:22:53 +0900

    e.g p53

--- chapter2/executable.rb
+++ chapter2/executable.rb
@@ -1,4 +1,10 @@
 require "./gear.rb"
 
-puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11).cog
 puts Gear.new(30, 27).ratio
+
+## 初期化パラメータ不足
+# Traceback (most recent call last):
+#         2: from executable.rb:3:in `<main>'
+#         1: from executable.rb:3:in `new'
+# /Users/mashimohroki/Desktop/Programming/Ruby /PracticalOOD_in_Ruby/chapter2/gear.rb:3:in `initialize': wrong number of arguments (given 4, expected 2) (ArgumentError)
--- chapter2/gear.rb
+++ chapter2/gear.rb
@@ -1,11 +1,21 @@
 class Gear
-  attr_reader :chainring, :cog
+  ## アクセサとは: https://qiita.com/yoshimitsu41/items/5f174ff74d8e8b15b0be
+  attr_reader :chainring, :cog ## インスタンス変数の読み取りメゾッドの提供
   def initialize(chainring, cog)
     @chainring = chainring
     @cog =       cog
   end
-  
+
   def ratio
+    # attr_readerによってinitializeで定義された@(インスタンス変数)のアクセサが提供されるので、@が不要になる
     chainring / cog.to_f ## convert to float
+  end
+  
+  def gear_inches
+    ratio * diameter
+  end
+  
+  def diameter
+    rim + (tire * 2)
   end
 end
