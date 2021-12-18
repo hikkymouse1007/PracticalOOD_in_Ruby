8b08038583acbaa4e1c1ba65ff51b051ad9b76e4
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-12-18 14:12:39 +0900

    finish chapter 8

--- chapter8/executable.rb
+++ chapter8/executable.rb
@@ -1,6 +1,7 @@
 require './bicycle.rb'
 require './parts.rb'
 require './part.rb'
+require './parts_factory.rb'
 require './road_bike_parts.rb'
 require './mountain_bike_parts.rb'
 
@@ -20,38 +21,75 @@ p front_shock = Part.new(
   needs_spare: false,
 )
 
-p road_bike_parts = Parts.new([chain, road_tire, tape])
+# p road_bike_parts = Parts.new([chain, road_tire, tape])
+
+# p road_bike = Bicycle.new(
+#   size: 'L',
+#   parts: Parts.new([
+#     chain,
+#     road_tire,
+#     tape,
+#   ])
+# )
+
+# p road_bike = Bicycle.new(
+#   size: 'L',
+#   parts: road_bike_parts
+# )
+
+# p road_bike.size
+# p road_bike.spares
+
+# p mountain_bike = Bicycle.new(
+#   size: 'L',
+#   parts: Parts.new([
+#     chain,
+#     mountain_tire,
+#     front_shock,
+#     rear_shock
+#   ])
+# )
+
+# p mountain_bike.size
+# p mountain_bike.spares
+# p mountain_bike.spares.size
+# p mountain_bike.parts.size
+# p "mountain_bike.parts.class: #{mountain_bike.parts.class}" #Parts
+# p "mountain_bike.parts.spares.class: #{mountain_bike.parts.spares.class}" #Array
+
+p road_config =
+[
+  ['chain', '10-speed'],
+  ['tire_size', '23'],
+  ['tape_color', 'red']
+]
+
+p mountain_config =
+[
+  ['chain', '10-speed'],
+  ['tire_size', '2.1'],
+  ['front_shock', 'Manitou', false,],
+  ['rear_shock', 'Fox']
+]
+
+p recumbent_config =
+[
+  ['chain', '9-speed'],
+  ['tire_size', '28'],
+  ['flag', 'tall and orange']
+]
 
 p road_bike = Bicycle.new(
   size: 'L',
-  parts: Parts.new([
-    chain,
-    road_tire,
-    tape,
-  ])
+  parts: PartsFactory.build(road_config)
 )
-
-p road_bike = Bicycle.new(
-  size: 'L',
-  parts: road_bike_parts
-)
-
-p road_bike.size
-p road_bike.spares
 
 p mountain_bike = Bicycle.new(
   size: 'L',
-  parts: Parts.new([
-    chain,
-    mountain_tire,
-    front_shock,
-    rear_shock
-  ])
+  parts: PartsFactory.build(mountain_config)
 )
 
-p mountain_bike.size
-p mountain_bike.spares
-p mountain_bike.spares.size
-p mountain_bike.parts.size
-p "mountain_bike.parts.class: #{mountain_bike.parts.class}" #Parts
-p "mountain_bike.parts.spares.class: #{mountain_bike.parts.spares.class}" #Array
+p recumbent_bike = Bicycle.new(
+  size: 'L',
+  parts: PartsFactory.build(recumbent_config)
+)
---
+++ chapter8/parts_factory.rb
@@ -0,0 +1,18 @@
+require 'ostruct'
+module PartsFactory
+  def self.build(config, parts_class = Parts)
+    parts_class.new(
+      config.collect {|part_config|
+        creat_part(part_config)
+      }
+    )
+  end
+  
+  def self.creat_part(part_config)
+    OpenStruct.new(
+      name:        part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true)
+    )
+  end
+end
