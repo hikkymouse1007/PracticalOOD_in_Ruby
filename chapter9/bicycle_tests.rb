d893eec484bab5915836e011ad164c90c8748668
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-12-28 00:49:21 +0900

    [WIP] p286

--- chapter9/bicycle_tests.rb
+++ chapter9/bicycle_tests.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require './bicycle.rb'
 require './road_bike.rb'
+require './mountain_bike.rb'
 require './bicycle_interface_test.rb'
+require './bicycle_subclass_test.rb'
 
 class BicycleTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
@@ -14,5 +16,13 @@ class RoadBikeTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
   def setup
     @bike = @object = RoadBike.new
+  end
+end
+
+class MountainBikeTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+  include BicycleSubclassTest
+  def setup
+    @bike = @object = MountainBike.new
   end
 end
